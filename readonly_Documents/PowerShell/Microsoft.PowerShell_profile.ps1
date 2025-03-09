e9bb7a4fe9dfb351e08c46e37e730d819cb6b76e
patricesos <patricesos7@gmail.com>
2025-03-09 21:20:43 +0000

    update

---
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,18 @@
+Import-Module Catppuccin
+
+$Flavor = $Catppuccin['Frappe']
+
+oh-my-posh --init --shell pwsh --config "$env:POSH_THEMES_PATH\amro.omp.json" | Invoke-Expression
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
+
+New-Alias pe pyenv
+
+
+function SymLink {
+	param (
+		$Target, $Path
+	)
+	New-Item -ItemType SymbolicLink -Path $Path -Target $Target
+}
+
+
---
+++ readonly_Documents/PowerShell/profile.ps1
@@ -0,0 +1,79 @@
+# My powershell custom function
+
+hugo completion powershell | Out-String | Invoke-Expression
+
+function pw { pwsh -nol }
+
+function moho {
+    C:\Program` Files\Moho` 14\Moho.exe
+}
+
+function env { .\.venv\Scripts\Activate.ps1 }
+
+function create-env { python` -m` venv` .venv }
+
+function lz { $env:NVIM_APPNAME = "lazy"; nvim }
+
+function nc { $env:NVIM_APPNAME = "nv-code"; nvim }
+
+function anime { fastanime anilist }
+
+function mpv-edit { nvim "F:\mpv\portable_config" }
+
+function ll { eza -al }
+
+function SymLink {
+    param ( $Target, $Path )
+    New-Item -ItemType SymbolicLink -Path $Path -Target $Target
+}
+
+function pipe { python` -m` pip }
+
+function c. { code . }
+
+function audio {
+    param($link)
+    yt-dlp -o $env:userprofile/downloads/music/"%(title)s.%(ext)s" -x --audio-format mp3 $link
+}
+
+function hd {
+    param($link)
+    yt-dlp -o $env:userprofile/downloads/video/"%(title)s.%(ext)s" -f "140+136" $link
+}
+
+function fhd {
+    param($link)
+    yt-dlp -o $env:userprofile/downloads/video/"%(title)s.%(ext)s" -f "140+137" $link
+}
+
+function insta {
+    param($link)
+    yt-dlp -o $env:userprofile/downloads/video/"%(title)s.%(ext)s" -f "ba+bv" $link --cookies-from-browser firefox
+}
+
+function video {
+    param($link)
+    yt-dlp -o $env:userprofile/downloads/video/"%(title)s.%(ext)s" -f "ba+bv" $link
+}
+
+function global {
+    $globalVenvPath = "$HOME/.globalVenv"
+    if (Test-Path $globalVenvPath) {
+        & "$globalVenvPath/Scripts/Activate.ps1"
+    } else {
+        Write-Output "No virtual environment found in .venv or ~/.globalVenv"
+    }
+}
+
+function tr {
+    param($opt)
+    eza --icons=always -G -w 2 --follow-symlinks $opt
+}
+
+function gaffer-edit {
+    nvim f:/software/gaffer.bat
+}
+
+function glob {
+    dir
+}
