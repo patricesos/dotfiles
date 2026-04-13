9d014f61b4e8a5808a9250dd8d59744e34f5a0ea
dell <dell@192.168.1.69>
2026-04-13 12:01:35 +0000

    Update .config/qtile/config.py

--- dot_config/qtile/config.py
+++ dot_config/qtile/config.py
@@ -118,12 +118,12 @@ layouts = [
     # Try more layouts by unleashing below layouts.
     # layout.Stack(num_stacks=2),
     # layout.Bsp(),
-    # layout.Matrix(),
+    layout.Matrix(),
     # layout.MonadTall(),
     # layout.MonadWide(),
     # layout.RatioTile(),
     # layout.Tile(),
-    layout.TreeTab(),
+    # layout.TreeTab(),
     # layout.VerticalTile(),
     layout.Zoomy(),
 ]
