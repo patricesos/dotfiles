69de5329ac0a6d8168b0662a3cf0b3e56553fe8b
patsos <recycler1995@gmail.com>
2026-04-14 17:36:08 +0000

    Update bootstrap Add .config/doom/config.el Add .config/doom/custom.el Add .config/doom/init.el Add .config/doom/packages.el Update .config/nvim/lua/config/options.lua

---
+++ dot_config/doom/custom.el
@@ -0,0 +1,14 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("be0d9f0e72a4ebc4a59c382168921b082b4dc15844bdaf1353c08157806b3321" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,7 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
---
+
 local opt = vim.opt
 
 opt.wrap = true
@@ -29,14 +29,16 @@ if vim.g.neovide then
   vim.g.neovide_padding_left = 20
 
   vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
-
   vim.g.neovide_scale_factor = 1.0
+
   local change_scale_factor = function(delta)
     vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
   end
+
   vim.keymap.set("n", "<C-=>", function()
     change_scale_factor(1.25)
   end)
+
   vim.keymap.set("n", "<C-->", function()
     change_scale_factor(1 / 1.25)
   end)
